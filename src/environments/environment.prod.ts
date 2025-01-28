584bb1944d1dd25d1472859a6aade648b3ef4eda
anojoseph <anojoseph2011@gmail.com>
2025-01-29 00:20:25 +0530

    added env prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://your-production-api-url.com/api', // Replace with your production API base URL
+  baseUrl: 'https://billing-backend-node.onrender.com', // Replace with your production API base URL
 };
